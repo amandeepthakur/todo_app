8520b9b6d2d376b43c95d075269f7238d5d39fa5
amandeepthakur <aman914501@gmail.com>
2024-01-12 01:25:37 +0530

    fourth change

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class _ToDoListScreenState extends State<ToDoListScreen> {
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: Text('Add a new task'),
+          title: Text('Add To-Do'),
           content: TextField(
             controller: todoController,
           ),
